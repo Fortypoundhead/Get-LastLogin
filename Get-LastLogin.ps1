64459c1222f8fafabd00569f6138aa0e07f362d3
Derek Wirch <wirderek@amazon.com>
2024-02-01 08:26:33 -0800

    Update Formatting

--- Get-LastLogin.ps1
+++ Get-LastLogin.ps1
@@ -5,22 +5,26 @@ param
 
 Import-Module ActiveDirectory
 
-# It's necessary to contact all domain controllers to get the true 
-# last logon time of the specified user. First though, we must have
-# a list of the domain controllers.
+<#
+    It's necessary to contact all domain controllers to get the true 
+    last logon time of the specified user. First though, we must have
+    a list of the domain controllers.
+#>
 
 $dcs = Get-ADDomainController -Filter {Name -like "*"}
 
-# set the time var for a number
+#   set the time var for a number
 
 $time = 0
 
 foreach($dc in $dcs)
 {
 
-    # iterate through the list of domain controllers, looking for the
-    # specified username. One we have the data, it is compared to the
-    # previous value for $time, and only the newer value is kept.
+    <#
+    iterate through the list of domain controllers, looking for the
+    specified username. One we have the data, it is compared to the
+    previous value for $time, and only the newer value is kept.
+#>
 
     $hostname = $dc.HostName
     $user = Get-ADUser $userName | Get-ADObject -Properties lastLogon
@@ -30,7 +34,7 @@ foreach($dc in $dcs)
     }
 }
 
-# convert the value to a human readable format, and display it on screen.
+#   convert the value to a human readable format, and display it on screen.
 
 $dt = [DateTime]::FromFileTime($time)
 Write-Host $username "last logged on at:" $dt 
